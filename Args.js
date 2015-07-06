d967f9fb7b2b3cd8802655ab1019ab5913ef1b3e
Jake Clarkson <jacobwclarkson@gmail.com>
2015-07-06 13:05:20 +0100

    AMD

--- Args.js
+++ Args.js
@@ -24,6 +24,7 @@ THE SOFTWARE.
 
 ;(function(name, context, definition) {
     if (typeof module !== 'undefined' && module.exports) { module.exports = definition(); }
+    else if (typeof define === 'function' && define.amd) { define(definition); }
     else { context[name] = definition(); }
 })('Args', this, function argsDefinition() {
 	"use strict";
