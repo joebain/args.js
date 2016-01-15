f12bedfded6be2a81c5edf052e86026d4c331308
Dian Fay <dian.m.fay@gmail.com>
2016-01-15 09:19:01 -0500

    conformance

--- Args.js
+++ Args.js
@@ -31,7 +31,7 @@ THE SOFTWARE.
 
 	if (!Array.isArray) {
 		Array.isArray = function(arg) {
-			return Object.prototype.toString.call(arg) === '[object Array]';
+			return Object.prototype.toString.call(arg) === "[object Array]";
 		};
 	}
 
